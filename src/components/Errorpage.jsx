2403e8147ac5d2f96ddfc1ffe6c4aa6bb55778d1
abundiko <abundiko111@gmail.com>
2023-08-07 13:48:38 +0100

    copied from next_branch

---
+++ src/components/Errorpage.jsx
@@ -0,0 +1,14 @@
+
+
+function PageNotFound() {
+    return (
+      <div className="Errorpage bg-info">
+            <h1>404</h1>
+            <p>OOPS, this page does not exit.
+                <span className="d-block"></span> Go to search to get see more interesting movies
+            </p>
+      </div>
+    );
+}
+
+export default PageNotFound;
